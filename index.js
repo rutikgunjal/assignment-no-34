43309e2f36b1ec43f215e781e886c1d01158d3e1
rutikgunjal <rutikgunjal121@email.com>
2024-07-29 14:55:36 +0530

    code for sum of odd number

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 
-//largest number
-let result = [99 , 53 , 105, 55 ];
+// //largest number
+// let result = [99 , 53 , 105, 55 ];
 
-let largest = result[0];
+// let largest = result[0];
 
-for(const num of result){
-    if(num>largest){
-        largest=num;
+// for(const num of result){
+//     if(num>largest){
+//         largest=num;
+//     }
+// }
+// console.log(`Largest Result:${largest}`);
+
+//sum of odd number
+let numbers = [105, 47, 70 , 33];
+
+let sum = 0;
+
+for(let i=0;i<numbers.length;i++){
+
+    if(numbers[i]%2==1){
+        sum += numbers[i];
     }
 }
-console.log(`Largest Result:${largest}`);
-
+console.log(`SUM:${sum}`)
